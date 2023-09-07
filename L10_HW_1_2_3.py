ab5a92093aa589da422c2225ec4222c7bf42c4ce
Kirill <100080763+Snakeinweb@users.noreply.github.com>
2023-09-07 22:26:33 +0700

    update

--- L10_HW_1_2_3.py
+++ L10_HW_1_2_3.py
@@ -14,22 +14,39 @@ name_pets = input("Как зовут питомца?: ")
 who_pets = input("Кто ваш питомец?: ")
 age_pets = int(input("Сколько лет питомцу?: "))
 
+# Обработка правильного соответствия с указанным возрастом выводит год, годаили лет до 100
+
 str_age_pets = str(age_pets)
 s = len(str_age_pets)
+
 s_last = int(str_age_pets[s-1])
 if s_last == 2 and age_pets != 12:
     f_age = "года"
+elif s_last == 3 and age_pets != 13:
+    f_age = "года"
+elif s_last == 4 and age_pets != 14:
+    f_age = "года"
+elif s_last == 1 and age_pets != 11:
+    f_age = "год"
 else:
-    if s_last == 3 and age_pets != 13:
-        f_age = "года"
-    else:
-        if s_last == 4 and age_pets != 14:
-            f_age = "года"
-        else:    
-            if s_last == 1 and age_pets != 11:
-                f_age = "год"
-            else:
-                f_age = "лет"
+    f_age = "лет"
+
+# Обработка правильного соответствия с указанным возрастом выводит год, годаили лет больше 100
+    
+s_2_last = int(str_age_pets[s-2:])
+if s_last == 2 and s_2_last != 12:
+    f_age = "года"
+elif s_last == 3 and s_2_last != 13:
+    f_age = "года"
+elif s_last == 4 and s_2_last != 14:
+    f_age = "года"
+elif s_last == 1 and s_2_last != 11:
+    f_age = "год"
+else:
+    f_age = "лет"
+
+
+
 pets = {
     name_pets : {
         b : who_pets,
@@ -41,3 +58,6 @@ pets = {
 print ("Это", who_pets, "по кличке", name_pets + ". Возраст:", age_pets, f_age)
 print (pets)
 
+print (f' Это {pets[name_pets][b]} по кличке {pets[name_pets]}. Возраст: {pets[name_pets][c]} {f_age}. Иммя владельца: {pets[name_pets][d]}')
+
+# Это желторотый питон по кличке "Каа". Возраст питомца: 19 лет. Имя владельца: Саша
