9757fd669b186093cead90b4bf1c8918d83394ac
Kirill <100080763+Snakeinweb@users.noreply.github.com>
2023-09-07 10:56:59 +0700

    обновление

--- L10_HW_1_2_3.py
+++ L10_HW_1_2_3.py
@@ -17,12 +17,19 @@ age_pets = int(input("Сколько лет питомцу?: "))
 str_age_pets = str(age_pets)
 s = len(str_age_pets)
 s_last = int(str_age_pets[s-1])
-if s_last == 2 or 3 or 4 and age_pets != 12 or 13 or 14:
+if s_last == 2 and age_pets != 12:
     f_age = "года"
-elif s_last == 1 and age_pets != 11:
-    f_age = "год"
 else:
-    f_age = "лет"
+    if s_last == 3 and age_pets != 13:
+        f_age = "года"
+    else:
+        if s_last == 4 and age_pets != 14:
+            f_age = "года"
+        else:    
+            if s_last == 1 and age_pets != 11:
+                f_age = "год"
+            else:
+                f_age = "лет"
 pets = {
     name_pets : {
         b : who_pets,
@@ -30,6 +37,7 @@ pets = {
         d : name_owner
         }
         }
-print ("Это", who_pets, "по кличке", name_pets + ". Возраст:", age_pets, f_age)
 
+print ("Это", who_pets, "по кличке", name_pets + ". Возраст:", age_pets, f_age)
+print (pets)
 
